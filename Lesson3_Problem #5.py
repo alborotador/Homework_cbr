4281e25ec9098e079c7c2ab62d33032570c49fb1
alborotador <129375281+alborotador@users.noreply.github.com>
2023-05-08 19:51:44 +0300

    Delete Lesson3_Problem #5.py

--- Lesson3_Problem #5.py
+++
@@ -1,9 +0,0 @@
-# Problem #5. Факториал
-# Программа по вычислению факториала числа, заданного пользователем
-
-n = int(input())
-factorial = 1
-for i in range(1, n + 1):
-    print("factorial =", factorial, "*", i)
-    factorial *= i
-print("factorial =", factorial)
